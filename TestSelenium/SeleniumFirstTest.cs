6469e25a0838b4569f733114d0576c1844440e4e
D1-anna <diana.pavlovych@globallogic.com>
2023-05-22 17:37:42 +0300

    Add the browser Options; Add the Cookies read/delete.

--- TestSelenium/SeleniumFirstTest.cs
+++ TestSelenium/SeleniumFirstTest.cs
@@ -12,7 +12,10 @@ namespace TestSelenium
         [SetUp]
         public void Setup()
         {
-            driver = new EdgeDriver();
+            EdgeOptions options = new EdgeOptions();
+            options.AddArgument("start-maximized");
+            driver = new EdgeDriver(options);
+
             wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
         }
         [TearDown]
@@ -25,11 +28,23 @@ namespace TestSelenium
         public void Test1()
         {
             driver.Navigate().GoToUrl("https://www.office.com/");
+
+            // display all cookies that created from session start
+            foreach (var cookie in driver.Manage().Cookies.AllCookies)
+            {
+                Console.WriteLine($" driver coolies: {cookie}");
+            }
+           
+
             wait.Until(driver => driver.FindElement(By.Id("hero-section")));
             driver.FindElement(By.Id("c-shellmenu_0")).Click();
             wait.Until(driver => driver.FindElement(By.Id("shellmenu_34")));            
             driver.FindElement(By.XPath("//a[contains(text(),'Clipchamp')]")).Click();
             wait.Until(driver => driver.FindElement(By.ClassName("h1")));
+            
+            // delete all stored cookies during session
+            driver.Manage().Cookies.DeleteAllCookies();
+            
 
         }
     }
